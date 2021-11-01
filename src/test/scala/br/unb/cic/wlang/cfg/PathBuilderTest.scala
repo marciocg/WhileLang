d089ecda179cec56e679729c1afc405036dd0caa
Márcio Conceição Goulart <marcio.goulart@gmail.com>
2021-11-01 10:48:12 -0300

    Uses CFGBuilder.interflow on the Fibonacci.wp and adjusts the expected paths in the test case

--- src/test/scala/br/unb/cic/wlang/cfg/PathBuilderTest.scala
+++ src/test/scala/br/unb/cic/wlang/cfg/PathBuilderTest.scala
@@ -7,15 +7,23 @@ import br.unb.cic.wlang.WhileProgram
 import br.unb.cic.wlang.parser.{ResourceHandle, WhileProgramParser}
 import br.unb.cic.wlang.semantics.{StructuralSemantics, TC}
 
-
 class PathBuilderTest extends AnyFunSuite {
 
-  val flow1: CFG = Set((1,2), (2,3), (3,4), (4,5), (5,3), (3,6))
-  val flow2: CFG = Set((0,1), (0,3), (1,3), (2,0), (2,1))
+  val flow1: CFG = Set((1, 2), (2, 3), (3, 4), (4, 5), (5, 3), (3, 6))
+  val flow2: CFG = Set((0, 1), (0, 3), (1, 3), (2, 0), (2, 1))
   val flow3: CFG = Set(
-    (1,2), (2,3), (3,8),
-    (2,4), (4,1), (8,5), (5,6), (6,1), (8,7), (7,8),
-    (9, 1), (8,10)
+    (1, 2),
+    (2, 3),
+    (3, 8),
+    (2, 4),
+    (4, 1),
+    (8, 5),
+    (5, 6),
+    (6, 1),
+    (8, 7),
+    (7, 8),
+    (9, 1),
+    (8, 10)
   )
   val interFlow: InterCFG = Set((9, 1, 8, 10), (4, 1, 8, 5), (6, 1, 8, 7))
 
@@ -25,45 +33,44 @@ class PathBuilderTest extends AnyFunSuite {
   }
 
   test("Test for path 1 2 flow1") {
-    val expected = List(1,2)
+    val expected = List(1, 2)
     assert(path(1, 2, flow1).contains(expected))
   }
 
   test("Test for path 1 3 flow1") {
-    val expected1 = List(1,2,3)
-    val expected2 = List(1,2,3,4,5,3)
+    val expected1 = List(1, 2, 3)
+    val expected2 = List(1, 2, 3, 4, 5, 3)
     assert(path(1, 3, flow1).contains(expected1))
     assert(path(1, 3, flow1).contains(expected2))
   }
 
   test("Test for path 2 3 flow1") {
-    val expected1 = List(2,3)
+    val expected1 = List(2, 3)
     val expected2 = List(2, 3, 4, 5, 3)
     assert(path(2, 3, flow1).contains(expected1))
     assert(path(2, 3, flow1).contains(expected2))
   }
 
   test("Test for path 1 4 flow1") {
-    val expected1 = List(1,2,3,4)
-    val expected2 = List(1,2,3,4,5,3,4)
+    val expected1 = List(1, 2, 3, 4)
+    val expected2 = List(1, 2, 3, 4, 5, 3, 4)
     assert(path(1, 4, flow1).contains(expected1))
     assert(path(1, 4, flow1).contains(expected2))
   }
 
   test("Test for path 1 5 flow1") {
-    val expected1 = List(1,2,3,4,5)
-    val expected2 = List(1,2,3,4,5,3,4,5)
+    val expected1 = List(1, 2, 3, 4, 5)
+    val expected2 = List(1, 2, 3, 4, 5, 3, 4, 5)
     assert(path(1, 5, flow1).contains(expected1))
     assert(path(1, 5, flow1).contains(expected2))
   }
 
   test("Test for path 1 6 flow1") {
-    val expected1 =List(1,2,3,4,5,3,6)
-    val expected2 = List(1,2,3,6)
+    val expected1 = List(1, 2, 3, 4, 5, 3, 6)
+    val expected2 = List(1, 2, 3, 6)
     assert(path(1, 6, flow1).contains(expected1))
     assert(path(1, 6, flow1).contains(expected2))
   }
-
 
   // val flow2: CFG = Set((2,0), (2,1), (0,3), (0,1), (1,3))
 
@@ -74,21 +81,20 @@ class PathBuilderTest extends AnyFunSuite {
     assert(path(2, 3, flow2).contains(expected2))
   }
 
-
   test("Test for path 9 10 flow3") {
     val expected1 = List(9, 1, 2, 4, 1, 2, 3, 8, 5, 6, 1, 2, 3, 8, 7, 8, 10)
     val expected2 = List(9, 1, 2, 4, 1, 2, 3, 8, 10)
     assert(path(9, 10, flow3).contains(expected1))
     assert(path(9, 10, flow3).contains(expected2))
-    assert(completePath(expected1,flow3, interFlow))
-    assert(!completePath(expected2,flow3, interFlow))
+    assert(completePath(expected1, flow3, interFlow))
+    assert(!completePath(expected2, flow3, interFlow))
   }
 
   test("Test for complete path") {
     val expected1 = List(9, 1, 2, 4, 1, 2, 3, 8, 5, 6, 1, 2, 3, 8, 7, 8, 10)
     val expected2 = List(9, 1, 2, 4, 1, 2, 3, 8, 10)
-    assert(completePath(expected1,flow3, interFlow))
-    assert(!completePath(expected2,flow3, interFlow))
+    assert(completePath(expected1, flow3, interFlow))
+    assert(!completePath(expected2, flow3, interFlow))
   }
 
   test("Test for complete path parsing the fibonacci program") {
@@ -113,19 +119,26 @@ class PathBuilderTest extends AnyFunSuite {
       case _        => fail()
     }
 
-    //val interflow3: InterCFG = Set((9, 1, 8, 10), (4, 1, 8, 5), (6, 1, 8, 7))
     val cfgFlow = CFGBuilder.flow(wp)
-    //cfgFlow.foreach(par => println(s"Par (Label,Label): $par"))    
+    val intercfgFlow = CFGBuilder.interFlow(wp)
 
-    val expected1 = List(9, 1, 2, 4, 1, 2, 3, 8, 5, 6, 1, 2, 3, 8, 7, 8, 10)
-    val expected2 = List(9, 1, 2, 4, 1, 2, 3, 8, 10)
-    val expected3 = List(9, 1, 2, 3, 8, 10)
-    assert(completePath(expected1,cfgFlow, interFlow))
-    assert(!completePath(expected2,cfgFlow, interFlow))
-    assert(completePath(expected3,cfgFlow, interFlow))
-    // val paths = path(9, 10, flow3)
-    // for (p <- paths) {
-    // completePaths(9, 10, p, interflow3)
-    // }
+    // flow3 = cfgFlow
+    // 1  = 14
+    // 2  = 7
+    // 3  = 8
+    // 4  = 9
+    // 5  = 10
+    // 6  = 12
+    // 7  = 13
+    // 8  = 15
+    // 9  = 17
+    // 10 = 18
+    
+    val expected1 = List(17, 14, 7, 9, 14, 7, 8, 15, 10, 12, 14, 7, 8, 15, 13, 15, 18)
+    val expected2 = List(17, 14, 7, 9, 14, 7, 8, 15, 18)
+    val expected3 = List(17, 14, 7, 8, 15, 18)
+    assert(completePath(expected1, cfgFlow, intercfgFlow))
+    assert(!completePath(expected2, cfgFlow, intercfgFlow))
+    assert(completePath(expected3, cfgFlow, intercfgFlow))
   }
 }
